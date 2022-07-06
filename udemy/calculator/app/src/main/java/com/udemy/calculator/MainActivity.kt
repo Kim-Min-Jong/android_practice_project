6d2ec5a1bd01466e68b9ee19beb148cda6e40b5f
minjong kim <alswhddkdl@gmail.com>
2022-07-06 15:37:40 +0900

    FEAT: make UI & impl CLEAR number

--- udemy/calculator/app/src/main/java/com/udemy/calculator/MainActivity.kt
+++ udemy/calculator/app/src/main/java/com/udemy/calculator/MainActivity.kt
@@ -2,10 +2,34 @@ package com.udemy.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private var tvInput: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tvInput = findViewById(R.id.tvInput)
     }
+    fun onDigit(view: View){
+        // 뷰를 버튼으로 취급해 text를 가져옴
+        tvInput?.append((view as Button).text)
+    }
+    fun onOperator(view: View){
+        Toast.makeText(this,"op clicked",Toast.LENGTH_SHORT).show()
+    }
+    fun onEqual(view: View){
+        Toast.makeText(this,"eq clicked",Toast.LENGTH_SHORT).show()
+    }
+    fun onClear(view: View){
+        tvInput?.text = ""
+    }
+    fun onDecimalPoint(view: View){
+        Toast.makeText(this,"button clicked",Toast.LENGTH_SHORT).show()
+    }
+
 }
