b90b06dfeb3e8d43d03bb2508c1cbde8618bc91e
minjong kim <alswhddkdl@gmail.com>
2022-07-06 20:32:28 +0900

    FEAT: make QuizQuestionActivity & Quiz Model

---
+++ udemy/quiz_app/app/src/main/java/com/udemy/quizapp/QuizQuestionActivity.kt
@@ -0,0 +1,13 @@
+package com.udemy.quizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QuizQuestionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz_question)
+
+        val questionsList = Constants.getQuestion()
+    }
+}
