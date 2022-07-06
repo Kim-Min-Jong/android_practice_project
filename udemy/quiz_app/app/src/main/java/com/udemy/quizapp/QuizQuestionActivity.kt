f706d303bce6aa2f5aa5a19288c9d52b3e43681b
minjong kim <alswhddkdl@gmail.com>
2022-07-06 22:41:41 +0900

    FEAT: choose the answer & go to next question

--- udemy/quiz_app/app/src/main/java/com/udemy/quizapp/QuizQuestionActivity.kt
+++ udemy/quiz_app/app/src/main/java/com/udemy/quizapp/QuizQuestionActivity.kt
@@ -2,33 +2,33 @@ package com.udemy.quizapp
 
 import android.graphics.Color
 import android.graphics.Typeface
-import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.ProgressBar
-import android.widget.TextView
+import android.widget.*
 import androidx.core.content.ContextCompat
 
 class QuizQuestionActivity : AppCompatActivity(), View.OnClickListener {
-
-    private var mCurrentPosition: Int = 1
-    private var mQuestionsList:ArrayList<Question>? = null
-    private var mSelectedOptionPosition : Int = 0
-
     private var progressBar: ProgressBar? = null
     private var tvProgress: TextView? = null
     private var tvQuestion: TextView? = null
     private var ivImage: ImageView? = null
-
     private var tvOptionOne: TextView? = null
     private var tvOptionTwo: TextView? = null
     private var tvOptionThree: TextView? = null
     private var tvOptionFour: TextView? = null
-    private var btnSubmit : Button? = null
+    private var buttonSubmit: Button? = null
 
+    // START
+    private var mCurrentPosition: Int = 1 // Default and the first question position
+    private var mQuestionsList: ArrayList<Question>? = null
+    // END
+
+    // START
+    private var mSelectedOptionPosition: Int = 0
+
+    // END
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_question)
@@ -37,103 +37,196 @@ class QuizQuestionActivity : AppCompatActivity(), View.OnClickListener {
         tvProgress = findViewById(R.id.tv_progress)
         tvQuestion = findViewById(R.id.tv_question)
         ivImage = findViewById(R.id.iv_image)
-
         tvOptionOne = findViewById(R.id.tv_option_one)
         tvOptionTwo = findViewById(R.id.tv_option_two)
         tvOptionThree = findViewById(R.id.tv_option_three)
         tvOptionFour = findViewById(R.id.tv_option_four)
-        btnSubmit = findViewById(R.id.btn_submit)
-
+        buttonSubmit = findViewById(R.id.btn_submit)
         mQuestionsList = Constants.getQuestion()
+
         setQuestion()
-
-    }
-
-    private fun setQuestion() {
-        mCurrentPosition = 1
-        val question = mQuestionsList!![mCurrentPosition - 1]
-
-        progressBar?.progress = mCurrentPosition
-        ivImage?.setImageResource(question.image)
-        tvProgress?.text = "$mCurrentPosition / ${progressBar?.max}"
-        tvQuestion?.text = question.question
-        tvOptionOne?.text = question.optionOne
-        tvOptionTwo?.text = question.optionTwo
-        tvOptionThree?.text = question.optionThree
-        tvOptionFour?.text = question.optionFour
 
         tvOptionOne?.setOnClickListener(this)
         tvOptionTwo?.setOnClickListener(this)
         tvOptionThree?.setOnClickListener(this)
         tvOptionFour?.setOnClickListener(this)
-        btnSubmit?.setOnClickListener(this)
+        buttonSubmit?.setOnClickListener(this)
 
-        if(mCurrentPosition == mQuestionsList!!.size){
-            btnSubmit?.text = "FINISH"
-        }else{
-            btnSubmit?.text = "SUBMIT"
-        }
     }
 
-    // 선택해제시 원래 옵션을 원래대로 바꾸기
-    private fun defaultOptionsView(){
+
+    /**
+     * A function for setting the question to UI components.
+     */
+    private fun setQuestion() {
+
+        val question: Question =
+            mQuestionsList!![mCurrentPosition - 1] // Getting the question from the list with the help of current position.
+        defaultOptionsView()
+        if (mCurrentPosition == mQuestionsList!!.size) {
+            buttonSubmit?.text = "FINISH"
+        } else {
+            buttonSubmit?.text = "SUBMIT"
+        }
+        progressBar?.progress =
+            mCurrentPosition // Setting the current progress in the progressbar using the position of question
+        tvProgress?.text =
+            "$mCurrentPosition" + "/" + progressBar?.max // Setting up the progress text
+
+        // Now set the current question and the options in the UI
+        tvQuestion?.text = question.question
+        ivImage?.setImageResource(question.image)
+        tvOptionOne?.text = question.optionOne
+        tvOptionTwo?.text = question.optionTwo
+        tvOptionThree?.text = question.optionThree
+        tvOptionFour?.text = question.optionFour
+    }
+
+    /**
+     * A function to set default options view when the new question is loaded or when the answer is reselected.
+     */
+    private fun defaultOptionsView() {
         val options = ArrayList<TextView>()
-        tvOptionOne?.let{
+        tvOptionOne?.let {
             options.add(0, it)
         }
-        tvOptionTwo?.let{
+        tvOptionTwo?.let {
             options.add(1, it)
         }
-        tvOptionThree?.let{
+        tvOptionThree?.let {
             options.add(2, it)
         }
-        tvOptionFour?.let{
+        tvOptionFour?.let {
             options.add(3, it)
         }
-        for(option in options){
-            option.setTextColor(Color.parseColor("#7a8089"))
+
+        for (option in options) {
+            option.setTextColor(Color.parseColor("#7A8089"))
             option.typeface = Typeface.DEFAULT
             option.background = ContextCompat.getDrawable(
-                this,
+                this@QuizQuestionActivity,
                 R.drawable.default_option_border_bg
             )
         }
     }
-    //옵션 선택시 기존것들은 원래대로하고 클릭한것 효과주기
-    private fun selectedOptionView(tv:TextView, selectedOptionNum: Int){
+
+    override fun onClick(view: View?) {
+        when (view?.id) {
+
+            R.id.tv_option_one -> {
+                tvOptionOne?.let {
+                    selectedOptionView(it, 1)
+                }
+
+            }
+
+            R.id.tv_option_two -> {
+                tvOptionTwo?.let {
+                    selectedOptionView(it, 2)
+                }
+
+            }
+
+            R.id.tv_option_three -> {
+                tvOptionThree?.let {
+                    selectedOptionView(it, 3)
+                }
+
+            }
+
+            R.id.tv_option_four -> {
+                tvOptionFour?.let {
+                    selectedOptionView(it, 4)
+                }
+
+            }
+            R.id.btn_submit -> {
+                if (mSelectedOptionPosition == 0) {
+                    mCurrentPosition++
+                    when {
+                        mCurrentPosition <= mQuestionsList!!.size -> {
+                            setQuestion()
+                        }
+                        else -> {
+                            Toast.makeText(
+                                this@QuizQuestionActivity,
+                                "You have successfully completed the quiz.",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                } else {
+                    val question = mQuestionsList?.get(mCurrentPosition - 1)
+
+                    // This is to check if the answer is wrong
+                    if (question!!.correctAnswer != mSelectedOptionPosition) {
+                        answerView(mSelectedOptionPosition, R.drawable.incorrect_option_border_bg)
+                    }
+
+                    // This is for correct answer
+                    answerView(question.correctAnswer, R.drawable.correct_option_border_bg)
+
+                    if (mCurrentPosition == mQuestionsList!!.size) {
+                        buttonSubmit?.text = "FINISH"
+                    } else {
+                        buttonSubmit?.text = "GO TO NEXT QUESTION"
+                    }
+
+                    mSelectedOptionPosition = 0
+                }
+            }
+        }
+    }
+
+    /**
+     * A function to set the view of selected option view.
+     */
+    private fun selectedOptionView(tv: TextView, selectedOptionNum: Int) {
+
         defaultOptionsView()
+
         mSelectedOptionPosition = selectedOptionNum
-        tv.setTextColor(Color.parseColor("#363a43"))
+
+        tv.setTextColor(
+            Color.parseColor("#363A43")
+        )
         tv.setTypeface(tv.typeface, Typeface.BOLD)
         tv.background = ContextCompat.getDrawable(
-            this,
+            this@QuizQuestionActivity,
             R.drawable.selected_option_border_bg
         )
     }
-    override fun onClick(view: View?) {
-        when(view?.id){
-            R.id.tv_option_one->{
-                tvOptionOne?.let{
-                    selectedOptionView(it, 1)
-                }
+
+    /**
+     * A function for answer view which is used to highlight the answer is wrong or right.
+     */
+    private fun answerView(answer: Int, drawableView: Int) {
+
+        when (answer) {
+
+            1 -> {
+                tvOptionOne?.background = ContextCompat.getDrawable(
+                    this@QuizQuestionActivity,
+                    drawableView
+                )
             }
-            R.id.tv_option_two->{
-                tvOptionTwo?.let{
-                    selectedOptionView(it, 2)
-                }
+            2 -> {
+                tvOptionTwo?.background = ContextCompat.getDrawable(
+                    this@QuizQuestionActivity,
+                    drawableView
+                )
             }
-            R.id.tv_option_three->{
-                tvOptionThree?.let{
-                    selectedOptionView(it, 3)
-                }
+            3 -> {
+                tvOptionThree?.background = ContextCompat.getDrawable(
+                    this@QuizQuestionActivity,
+                    drawableView
+                )
             }
-            R.id.tv_option_four->{
-                tvOptionFour?.let{
-                    selectedOptionView(it, 4)
-                }
-            }
-            R.id.btn_submit->{
-                //todo
+            4 -> {
+                tvOptionFour?.background = ContextCompat.getDrawable(
+                    this@QuizQuestionActivity,
+                    drawableView
+                )
             }
         }
     }
