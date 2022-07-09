ddf650b430c198fd6eef32c5c1a79c16e25a65e5
minjong kim <alswhddkdl@gmail.com>
2022-07-10 00:27:13 +0900

    FEAT: remove action bar & set view binding

--- udemy/7minutes_exercise_app/app/src/main/java/com/udemy/a7minutesworkout/MainActivity.kt
+++ udemy/7minutes_exercise_app/app/src/main/java/com/udemy/a7minutesworkout/MainActivity.kt
@@ -3,15 +3,25 @@ package com.udemy.a7minutesworkout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
+import com.udemy.a7minutesworkout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
 
-        val flStartButton : FrameLayout = findViewById(R.id.flStart)
-        flStartButton.setOnClickListener{
+        binding?.flStart?.setOnClickListener{
 
         }
+    }
+
+    // view binding 사용 시 오버라이드 하기
+    override fun onDestroy() {
+        super.onDestroy()
+        // 바인딩 해체를 안하면 memory leak이 일어날 수 있으므로 방지 차원
+        binding = null
     }
 }
