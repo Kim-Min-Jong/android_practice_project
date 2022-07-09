7dfe411f567cf9c22b55fb67a89af09edff1f3b0
minjong kim <alswhddkdl@gmail.com>
2022-07-09 23:39:39 +0900

    FEAT: setting start screen UI

--- udemy/7minutes_exercise_app/app/src/main/java/com/udemy/a7minutesworkout/MainActivity.kt
+++ udemy/7minutes_exercise_app/app/src/main/java/com/udemy/a7minutesworkout/MainActivity.kt
@@ -2,10 +2,16 @@ package com.udemy.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val flStartButton : FrameLayout = findViewById(R.id.flStart)
+        flStartButton.setOnClickListener{
+
+        }
     }
 }
