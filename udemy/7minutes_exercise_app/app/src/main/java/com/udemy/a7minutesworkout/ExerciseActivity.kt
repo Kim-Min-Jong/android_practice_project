a22b409062e1a5066baa6edcebd3fbdd6201a1b7
minjong kim <alswhddkdl@gmail.com>
2022-07-10 23:52:11 +0900

    FEAT: apply tts to exercise name

--- udemy/7minutes_exercise_app/app/src/main/java/com/udemy/a7minutesworkout/ExerciseActivity.kt
+++ udemy/7minutes_exercise_app/app/src/main/java/com/udemy/a7minutesworkout/ExerciseActivity.kt
@@ -3,11 +3,15 @@ package com.udemy.a7minutesworkout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.speech.tts.TextToSpeech
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import com.udemy.a7minutesworkout.databinding.ActivityExerciseBinding
+import java.util.*
+import kotlin.collections.ArrayList
 
-class ExerciseActivity : AppCompatActivity() {
+class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
     private var binding: ActivityExerciseBinding? = null
 
     // 대기용
@@ -22,6 +26,8 @@ class ExerciseActivity : AppCompatActivity() {
     private var exerciseList: ArrayList<ExerciseModel>? = null
     private var currentExercisePosition = -1
 
+    //tts
+    private var tts: TextToSpeech? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityExerciseBinding.inflate(layoutInflater)
@@ -35,6 +41,8 @@ class ExerciseActivity : AppCompatActivity() {
         }
 
         exerciseList = Constants.defaultExerciseList()
+
+        tts = TextToSpeech(this, this)
 
         // 액션바 설정
         binding?.toolbarExercise?.setNavigationOnClickListener {
@@ -83,6 +91,8 @@ class ExerciseActivity : AppCompatActivity() {
             exerciseTimer?.cancel()
             exerciseProgress = 0
         }
+        //tts 설정
+        speakOut(exerciseList!![currentExercisePosition].name)
 
         // 데이터 바인딩
         binding?.ivImage?.setImageResource(exerciseList!![currentExercisePosition].image)
@@ -145,7 +155,27 @@ class ExerciseActivity : AppCompatActivity() {
             exerciseProgress = 0
         }
 
-
+        if(tts != null){
+            tts?.stop()
+            tts?.shutdown()
+        }
         binding = null
     }
+
+    override fun onInit(status: Int) {
+        if(status == TextToSpeech.SUCCESS){
+            val result = tts!!.setLanguage(Locale.KOREA)
+            if(result == TextToSpeech.LANG_MISSING_DATA
+                || result == TextToSpeech.LANG_NOT_SUPPORTED){
+                Log.e("TTS","not supported")
+            }else{
+                Log.e("TTS","init failed")
+            }
+        }
+    }
+    private fun speakOut(text:String){
+        // queue-flush -- 이전 입력 문장 삭제  add -- 이전문장에 이어서 추가
+        tts?.speak(text, TextToSpeech.QUEUE_FLUSH,null,"")
+    }
+
 }
