53fc46994768682216f8d1bd9c5e0d004e23626a
minjong kim <alswhddkdl@gmail.com>
2022-07-15 17:51:49 +0900

    FEAT: add gallery and camera selection function

--- udemy/Happy_place_app/app/src/main/java/com/udemy/happyplaces/AddHappyPlaceActivity.kt
+++ udemy/Happy_place_app/app/src/main/java/com/udemy/happyplaces/AddHappyPlaceActivity.kt
@@ -1,16 +1,20 @@
 package com.udemy.happyplaces
 
 import android.Manifest
+import android.app.Activity
 import android.app.AlertDialog
 import android.app.DatePickerDialog
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Matrix
 import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.provider.Settings
 import android.view.View
-import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.MultiplePermissionsReport
@@ -25,6 +29,29 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
     private var binding: ActivityAddHappyPlaceBinding? = null
     private var cal = Calendar.getInstance()
     private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
+    // deprecated 된 startActivityForResult 대체 수단
+    val getGalleryImageLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val uri = result.data?.data // 선택한 이미지의 주소(상대경로)
+
+                binding?.ivPlaceImage?.setImageURI(uri!!)
+            }
+        }
+    val takeCameraImageLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                var bitmap = result.data?.extras?.get("data") as Bitmap // 선택한 이미지의 주소(상대경로)
+                val matrix = Matrix()
+                matrix.postRotate(90f)
+                val rotatedBitmap = Bitmap.createBitmap(bitmap,0,0,
+                        bitmap.width, bitmap.height, matrix, true)
+                bitmap.recycle()
+                binding?.ivPlaceImage?.setImageBitmap(rotatedBitmap)
+            }
+        }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddHappyPlaceBinding.inflate(layoutInflater)
@@ -69,26 +96,48 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
                         _, which ->
                         when(which){
                             0 -> choosePhotoFroGallery()
-                            1 -> {
-
-                            }
+                            1 -> takePhotoFromCamera()
                         }
                 }
                 pictureDialog.create().show()
             }
         }
     }
-    private fun choosePhotoFroGallery(){
-        // 권한 설정 서드파티 라이브러리
+
+    private fun takePhotoFromCamera(){
         Dexter.withActivity(this).withPermissions(
             Manifest.permission.WRITE_EXTERNAL_STORAGE,
             Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.CAMERA
         ).withListener(object: MultiplePermissionsListener {
             // 권한이 확인되면
-            override fun onPermissionsChecked(report: MultiplePermissionsReport) {
-                if(report.areAllPermissionsGranted()){
-                    Toast.makeText(this@AddHappyPlaceActivity,"permission granted", Toast.LENGTH_LONG).show()
+            override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                if(report!!.areAllPermissionsGranted()){
+                    val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+//                    startActivityForResult(intent, GALLERY)
+                    takeCameraImageLauncher.launch(intent)
+                }
+            }
+            // 권한이 필요한지 보여줘야함
+            override fun onPermissionRationaleShouldBeShown(permissions: MutableList<PermissionRequest>, token: PermissionToken) {
+                showRationaleDialogForPermissions()
+            }
+        }).onSameThread().check()
+    }
+
+    private fun choosePhotoFroGallery(){
+        // 권한 설정 서드파티 라이브러리
+        Dexter.withActivity(this).withPermissions(
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        ).withListener(object: MultiplePermissionsListener {
+            // 권한이 확인되면
+            override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                if(report!!.areAllPermissionsGranted()){
+                    val intent = Intent(Intent.ACTION_PICK,
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+//                    startActivityForResult(intent, GALLERY)
+                    getGalleryImageLauncher.launch(intent)
                 }
             }
             // 권한이 필요한지 보여줘야함
@@ -120,5 +169,11 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
         val format = "yyyy.MM.dd"
         val sdf = SimpleDateFormat(format, Locale.getDefault())
         binding?.etDate?.setText(sdf.format(cal.time).toString())
+    }
+
+    companion object {
+        private const val GALLERY = 1
+        private const val CAMERA = 2
+
     }
 }
