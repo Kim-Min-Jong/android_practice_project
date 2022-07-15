a0a4980ff0fb8333f58ab2faa6557113492f4faf
minjong kim <alswhddkdl@gmail.com>
2022-07-15 20:23:20 +0900

    FEAT: read data from Local DB

--- udemy/Happy_place_app/app/src/main/java/com/udemy/happyplaces/activities/MainActivity.kt
+++ udemy/Happy_place_app/app/src/main/java/com/udemy/happyplaces/activities/MainActivity.kt
@@ -3,7 +3,9 @@ package com.udemy.happyplaces.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.udemy.happyplaces.database.DatabaseHandler
 import com.udemy.happyplaces.databinding.ActivityMainBinding
+import com.udemy.happyplaces.models.HappyPlaceModel
 
 class MainActivity : AppCompatActivity() {
     private var binding: ActivityMainBinding? = null
@@ -16,5 +18,15 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, AddHappyPlaceActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    // happy place를 가져오는 함수
+    private fun getHappyPlacesListFromLocalDB(){
+        val dbHandler = DatabaseHandler(this)
+        // 읽어온 것을 리스트로 저장
+        val getHappyPlaceList: ArrayList<HappyPlaceModel> =
+            dbHandler.getHappyPlacesList()
+
+
     }
 }
--- udemy/Happy_place_app/app/src/main/java/com/udemy/happyplaces/database/DatabaseHandler.kt
+++ udemy/Happy_place_app/app/src/main/java/com/udemy/happyplaces/database/DatabaseHandler.kt
@@ -2,7 +2,9 @@ package com.udemy.happyplaces.database
 
 import android.content.ContentValues
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteException
 import android.database.sqlite.SQLiteOpenHelper
 import com.udemy.happyplaces.models.HappyPlaceModel
 
@@ -61,5 +63,34 @@ class DatabaseHandler(context: Context) :
         return result
     }
 
+    fun getHappyPlacesList(): ArrayList<HappyPlaceModel>{
+        val happyPlaceList: ArrayList<HappyPlaceModel> = ArrayList()
+        val selectQuery = "select * from $TABLE_HAPPY_PLACE"
+        val db = this.readableDatabase
 
+        try{
+            // 테이블에서 행열 지정하려면 '커서'가 필요함
+            val cursor: Cursor = db.rawQuery(selectQuery, null)
+            if(cursor.moveToFirst()){
+                do{
+                    val place = HappyPlaceModel(
+                        cursor.getInt(cursor.getColumnIndexOrThrow(KEY_ID)),
+                        cursor.getString(cursor.getColumnIndexOrThrow(KEY_TITLE)),
+                        cursor.getString(cursor.getColumnIndexOrThrow(KEY_IMAGE)),
+                        cursor.getString(cursor.getColumnIndexOrThrow(KEY_DESCRIPTION)),
+                        cursor.getString(cursor.getColumnIndexOrThrow(KEY_DATE)),
+                        cursor.getString(cursor.getColumnIndexOrThrow(KEY_LOCATION)),
+                        cursor.getDouble(cursor.getColumnIndexOrThrow(KEY_LATITUDE)),
+                        cursor.getDouble(cursor.getColumnIndexOrThrow(KEY_LONGITUDE))
+                    )
+                    happyPlaceList.add(place)
+                }while(cursor.moveToNext())
+            }
+            cursor.close()
+        }catch (e: SQLiteException){
+            db.execSQL(selectQuery)
+            return ArrayList()
+        }
+        return happyPlaceList
+    }
 }
