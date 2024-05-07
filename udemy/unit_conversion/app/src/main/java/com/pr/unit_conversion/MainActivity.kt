7645ae06e5793cd93e0629b07c480e33ba9715e6
minjong kim <alswhddkdl@gmail.com>
2024-05-07 12:13:19 +0900

    feat: basic Composable UI

--- udemy/unit_conversion/app/src/main/java/com/pr/unit_conversion/MainActivity.kt
+++ udemy/unit_conversion/app/src/main/java/com/pr/unit_conversion/MainActivity.kt
@@ -3,8 +3,11 @@ package com.pr.unit_conversion
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,6 +19,19 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+
+        }
+    }
+}
+
+@Composable
+fun UnitConverter() {
+    // UI 열 형태로 쌓기
+    Column {
+        Text(text = "Unit Converter")
+
+        // 그 안에서 Row 가로행 컴포넌트 생성
+        Row {
 
         }
     }
