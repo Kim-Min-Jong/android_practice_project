a2eefb70648107439791c21144d8ff1bcdbeba6b
minjong kim <alswhddkdl@gmail.com>
2024-05-07 16:29:49 +0900

    feat: box & dropdown layout - 2 (horizontal, vertical UI sorting)

--- udemy/unit_conversion/app/src/main/java/com/pr/unit_conversion/MainActivity.kt
+++ udemy/unit_conversion/app/src/main/java/com/pr/unit_conversion/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -18,6 +19,7 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
@@ -36,7 +38,12 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun UnitConverter() {
     // UI 열 형태로 쌓기
-    Column {
+    Column(
+        // 요소 정렬
+        modifier = Modifier.fillMaxSize(), // 전체 채우기
+        verticalArrangement = Arrangement.Center, // 수직 중앙 정렬
+        horizontalAlignment = Alignment.CenterHorizontally // 수평 중앙 정렬
+    ) {
         Text(text = "Unit Converter")
         // (있어야하는 파라미터) 텍스트 및 텍스트가 입력되었을 때 실행될 콜백 등록
         OutlinedTextField(value = "", onValueChange = {
@@ -45,13 +52,13 @@ fun UnitConverter() {
         // 그 안에서 Row 가로행 컴포넌트 생성
         Row {
             // modifier에 조정가능한 빈 상자
-            Box(modifier = Modifier) {
+            Box {
                 Button(onClick = { /*TODO*/ }) {
                     Text(text = "Select")
                     Icon(imageVector = Icons.Default.ArrowDropDown, contentDescription = "")
                 }
             }
-            Box(modifier = Modifier) {
+            Box {
                 Button(onClick = { /*TODO*/ }) {
                     Text(text = "Select")
                     Icon(imageVector = Icons.Default.ArrowDropDown, contentDescription = "")
