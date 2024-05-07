2e64446feea41056f2750578d0f1d6a927022739
minjong kim <alswhddkdl@gmail.com>
2024-05-07 15:55:07 +0900

    feat: add OutLinedTextField & Anonymous function

--- udemy/unit_conversion/app/src/main/java/com/pr/unit_conversion/MainActivity.kt
+++ udemy/unit_conversion/app/src/main/java/com/pr/unit_conversion/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
@@ -24,12 +25,16 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun UnitConverter() {
     // UI 열 형태로 쌓기
     Column {
         Text(text = "Unit Converter")
-
+        // (있어야하는 파라미터) 텍스트 및 텍스트가 입력되었을 때 실행될 콜백 등록
+        OutlinedTextField(value = "", onValueChange = {
+            // 익명함수 스코프
+        })
         // 그 안에서 Row 가로행 컴포넌트 생성
         Row {
 
