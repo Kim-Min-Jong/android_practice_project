47f2b41ff2285f940f05b1087d6b8ebbf3cdbe8f
minjong kim <alswhddkdl@gmail.com>
2024-05-08 15:58:05 +0900

    feat: set custom text font style

--- udemy/unit_conversion/app/src/main/java/com/pr/unit_conversion/MainActivity.kt
+++ udemy/unit_conversion/app/src/main/java/com/pr/unit_conversion/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.pr.unit_conversion
 
 import android.os.Bundle
-import android.widget.Space
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
@@ -23,7 +21,6 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -32,12 +29,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.modifier.modifierLocalConsumer
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.pr.unit_conversion.ui.theme.Unit_conversionTheme
+import androidx.compose.ui.unit.sp
 import kotlin.math.roundToInt
 
 class MainActivity : ComponentActivity() {
@@ -83,6 +80,12 @@ fun UnitConverter() {
         outputValue = result.toString()
     }
 
+    val customTextStyle = TextStyle(
+        fontFamily = FontFamily.Monospace,
+        fontSize = 32.sp,
+        color = Color.Black
+    )
+
     // UI 열 형태로 쌓기
     Column(
         // 요소 정렬
@@ -92,7 +95,7 @@ fun UnitConverter() {
     ) {
         Text(
             text = "Unit Converter", modifier = Modifier.padding(8.dp),
-            style = MaterialTheme.typography.headlineLarge
+            style = customTextStyle
         )
         // 간격 컴포저블
         Spacer(modifier = Modifier.height(16.dp))
