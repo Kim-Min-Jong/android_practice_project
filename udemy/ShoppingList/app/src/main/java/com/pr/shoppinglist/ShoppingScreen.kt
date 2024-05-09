56e34fb5b933d0e1f0484ac34708d25df849548d
minjong kim <alswhddkdl@gmail.com>
2024-05-09 11:28:41 +0900

    [Shopping List] feat: create LazyColumn item UI - 1

--- udemy/ShoppingList/app/src/main/java/com/pr/shoppinglist/ShoppingScreen.kt
+++ udemy/ShoppingList/app/src/main/java/com/pr/shoppinglist/ShoppingScreen.kt
@@ -1,5 +1,7 @@
 package com.pr.shoppinglist
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -10,6 +12,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -22,6 +25,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.unit.dp
 import com.pr.shoppinglist.data.ShoppingItem
 
@@ -61,7 +66,11 @@ fun ShoppingListScreen() {
                 .padding(16.dp),
         ) {
             items(items) {
-
+                ShoppingListItem(
+                    item = it,
+                    onEditClick = { },
+                    onDeleteClick = { }
+                )
             }
         }
     }
@@ -122,5 +131,26 @@ fun ShoppingListScreen() {
             }
         )
     }
+}
 
+@Composable
+fun ShoppingListItem(
+    item: ShoppingItem,
+    onEditClick: () -> Unit,
+    onDeleteClick: () -> Unit,
+) {
+    Row(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+            .border(
+                border = BorderStroke(2.dp, Color.Cyan),
+                shape = RoundedCornerShape(20)
+            )
+    ) {
+        Text(
+            text = item.name,
+            modifier = Modifier.padding(8.dp)
+        )
+    }
 }
