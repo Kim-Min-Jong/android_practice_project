28313240456b44c41e0dfb76184ddc2d20182939
minjong kim <alswhddkdl@gmail.com>
2024-05-10 12:06:06 +0900

    [MVVM Counter] feat: set counter repository for data control

--- udemy/mvvm_counter/app/src/main/java/com/pr/mvvm_counter/CounterModel.kt
+++ udemy/mvvm_counter/app/src/main/java/com/pr/mvvm_counter/CounterModel.kt
@@ -3,3 +3,14 @@ package com.pr.mvvm_counter
 data class CounterModel(
     var count: Int
 )
+
+// 데이터를 관리하는 곳
+class CounterRepository {
+    private var counter = CounterModel(0)
+
+    fun getCounter() = counter
+
+    fun increment()  = counter.count++
+
+    fun decrement() = counter.count--
+}
