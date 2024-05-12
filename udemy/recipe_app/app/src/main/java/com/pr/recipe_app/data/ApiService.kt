861e0496681eab015d9dbd67fa8d1c7548d2ae7a
minjong kim <alswhddkdl@gmail.com>
2024-05-12 12:39:31 +0900

    [recipe app] feat: create retrofit object

---
+++ udemy/recipe_app/app/src/main/java/com/pr/recipe_app/data/ApiService.kt
@@ -0,0 +1,22 @@
+package com.pr.recipe_app.data
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import retrofit2.http.GET
+
+interface ApiService {
+
+    @GET("/categories.php")
+    suspend fun getCategories(): CategoriesResponses
+}
+
+// retrofit object
+private val retrofit = Retrofit.Builder()
+    .baseUrl("www.themealdb.com/api/json/v1/1")
+    .addConverterFactory(GsonConverterFactory.create())
+    .build()
+
+// exposed retroift object (create object)
+val retrofitService = retrofit.create(ApiService::class.java)
+
