09933adbff434233ffde7e65c204b991f685a0d0
minjong kim <alswhddkdl@gmail.com>
2024-05-12 12:20:52 +0900

    [recipe app] feat: set dependencies

--- udemy/recipe_app/app/build.gradle.kts
+++ udemy/recipe_app/app/build.gradle.kts
@@ -66,4 +66,14 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    // compose viewmodel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+
+    // retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // coil
+    implementation("io.coil-kt:coil-compose:2.4.0")
 }
