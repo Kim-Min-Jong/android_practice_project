b56befc760841d3877deef561a4e3a9a42341cd5
minjong kim <alswhddkdl@gmail.com>
2024-05-14 22:06:20 +0900

    [Navigation Sample] chore: move file

--- udemy/NavigationSample/app/src/main/java/com/pr/navigationsample/FirstScreen.kt
+++ udemy/NavigationSample/app/src/main/java/com/pr/navigationsample/FirstScreen.kt
@@ -1,9 +1,5 @@
 package com.pr.navigationsample
 
-import android.os.Bundle
-import android.widget.Space
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -12,34 +8,39 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.pr.navigationsample.ui.theme.NavigationSampleTheme
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            NavigationSampleTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FirstScreen() {
+    val name = remember {
+        mutableStateOf("")
+    }
 
-                }
-            }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "This is First Screen" , fontSize = 24.sp)
+        Spacer(modifier = Modifier.height(16.dp))
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(value =  name.value, onValueChange = { name.value = it })
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Go to Second Screen")
         }
     }
 }
+
 
