c7ff1bf7e8f49474d82714b5eb20e49731fab32b
minjong kim <alswhddkdl@gmail.com>
2024-05-15 11:58:32 +0900

    [recipe app] feat: set sealed class for navigation routing

---
+++ udemy/recipe_app/app/src/main/java/com/pr/recipe_app/Screen.kt
@@ -0,0 +1,10 @@
+package com.pr.recipe_app
+
+import androidx.compose.runtime.Composable
+
+// 탐색 종착지 클래스
+// sealed class를 통한 종류 제한
+sealed class Screen(val route: String) {
+    object RecipeScreen: Screen("recipescreen")
+    object DetailScreen: Screen("detailscreen")
+}
