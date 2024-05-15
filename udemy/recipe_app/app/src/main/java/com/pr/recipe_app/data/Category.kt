a996263e3bd9b41e8d9514bf4e6a9fa0d22409a9
minjong kim <alswhddkdl@gmail.com>
2024-05-15 12:23:21 +0900

    [recipe app] feat: make data class to parcelable

--- udemy/recipe_app/app/src/main/java/com/pr/recipe_app/data/Category.kt
+++ udemy/recipe_app/app/src/main/java/com/pr/recipe_app/data/Category.kt
@@ -1,11 +1,15 @@
 package com.pr.recipe_app.data
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Category(
     val idCategory: String,
     val strCategory: String,
     val strCategoryThumb: String,
     val strCategoryDescription: String
-)
+): Parcelable
 
 data class CategoriesResponses(
     val categories: List<Category>
