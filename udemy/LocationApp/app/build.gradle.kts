c185cbc5e52f66aa9663b9ea94cdd702adc87bcb
minjong kim <alswhddkdl@gmail.com>
2024-05-17 10:10:38 +0900

    [Location App] feat: add dependencies

--- udemy/LocationApp/app/build.gradle.kts
+++ udemy/LocationApp/app/build.gradle.kts
@@ -66,4 +66,10 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    // location
+    implementation("com.google.android.gms:play-services-location:21.2.0")
+
+    // compose viewmodel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.0")
 }
