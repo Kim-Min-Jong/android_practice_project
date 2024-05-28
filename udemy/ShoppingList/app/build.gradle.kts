6a105abb885aada7dfe26fcc0d3d94f6d5dc30ef
minjong kim <alswhddkdl@gmail.com>
2024-05-28 16:26:09 +0900

    [Shopping List - Advanced] feat: add dependencies

--- udemy/ShoppingList/app/build.gradle.kts
+++ udemy/ShoppingList/app/build.gradle.kts
@@ -66,4 +66,16 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    // map
+    implementation("com.google.maps.android:maps-compose:2.15.0")
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
+
+    // network
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // jetpack
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.0")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
 }
