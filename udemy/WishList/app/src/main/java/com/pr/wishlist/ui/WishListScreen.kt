114539ea5a01bdf61b8fee154511520ffcf4dc11
minjong kim <alswhddkdl@gmail.com>
2024-05-30 11:05:39 +0900

    [Wish List] feat: adding FAB(FloatingActionButton)

--- udemy/WishList/app/src/main/java/com/pr/wishlist/ui/WishListScreen.kt
+++ udemy/WishList/app/src/main/java/com/pr/wishlist/ui/WishListScreen.kt
@@ -4,11 +4,17 @@ import android.widget.Toast
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 
 // scaffold를 사용하여 탑바 만들기
 @OptIn(ExperimentalMaterial3Api::class)
@@ -24,6 +30,16 @@ fun HomeView(
         topBar = {
             AppBarView(title = "WishList") {
                 Toast.makeText(context, "Button Clicked", Toast.LENGTH_SHORT).show()
+            }
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                modifier = modifier.padding(20.dp),
+                contentColor = Color.White,
+                containerColor = Color.Black,
+                onClick = { /* navigae to add screen*/ } 
+            ) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = null)
             }
         }
     ) {
