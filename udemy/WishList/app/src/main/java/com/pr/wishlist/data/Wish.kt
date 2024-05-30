89ace49e0efbaf7a9c17959d8659ce0e50cd15f4
minjong kim <alswhddkdl@gmail.com>
2024-05-30 11:22:36 +0900

    [Wish List] feat: adding wish & wishItem

---
+++ udemy/WishList/app/src/main/java/com/pr/wishlist/data/Wish.kt
@@ -0,0 +1,7 @@
+package com.pr.wishlist.data
+
+data class Wish(
+    val id: Long = 0L,
+    val title: String = "",
+    val description: String = ""
+)
--- udemy/WishList/app/src/main/java/com/pr/wishlist/ui/WishListScreen.kt
+++ udemy/WishList/app/src/main/java/com/pr/wishlist/ui/WishListScreen.kt
@@ -1,20 +1,29 @@
 package com.pr.wishlist.ui
 
 import android.widget.Toast
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.pr.wishlist.data.Wish
 
 // scaffold를 사용하여 탑바 만들기
 @OptIn(ExperimentalMaterial3Api::class)
@@ -37,7 +46,7 @@ fun HomeView(
                 modifier = modifier.padding(20.dp),
                 contentColor = Color.White,
                 containerColor = Color.Black,
-                onClick = { /* navigae to add screen*/ } 
+                onClick = { /* navigae to add screen*/ }
             ) {
                 Icon(imageVector = Icons.Default.Add, contentDescription = null)
             }
@@ -49,6 +58,32 @@ fun HomeView(
                 .padding(it)
         ) {
 
+        }
+    }
+}
+
+@Composable
+fun WishItem(
+    modifier: Modifier = Modifier,
+    wish: Wish,
+    onClick: () -> Unit,
+) {
+    // CardView와 비슷한 컴포저블
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(top = 8.dp, start = 8.dp, end = 8.dp)
+            .clickable {
+                // 수정이나 작성하는 람다를 구체화
+                onClick()
+            },
+        elevation = CardDefaults.cardElevation(10.dp)
+    ) {
+        Column(
+            modifier = modifier.padding(16.dp)
+        ) {
+            Text(text = wish.title, fontWeight = FontWeight.ExtraBold)
+            Text(text = wish.description)
         }
     }
 }
