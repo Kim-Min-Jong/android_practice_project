4eb7785fcb0a0e89848ee55082d4f107e87f4054
minjong kim <alswhddkdl@gmail.com>
2024-05-31 11:41:40 +0900

    [Wish List] feat: adding wish & using snackbar

--- udemy/WishList/app/src/main/java/com/pr/wishlist/ui/AddEditDetailView.kt
+++ udemy/WishList/app/src/main/java/com/pr/wishlist/ui/AddEditDetailView.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.text.KeyboardOptions
@@ -12,9 +11,16 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -26,6 +32,8 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.pr.wishlist.R
 import com.pr.wishlist.WishViewModel
+import com.pr.wishlist.data.Wish
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -35,6 +43,12 @@ fun AddEditDetailView(
     viewModel: WishViewModel,
     navController: NavHostController
 ) {
+    var snackMessage by remember { mutableStateOf("") }
+
+    val scope = rememberCoroutineScope()
+
+    // state
+    val snackbarHostState = remember { SnackbarHostState() }
     Scaffold(
         // 탑바 재사용
         topBar = {
@@ -46,7 +60,9 @@ fun AddEditDetailView(
                 // 이전으로 이동 (뒤로가기)
                 navController.navigateUp()
             }
-        }
+        },
+        // 스낵바의 state를 등록
+        snackbarHost = { SnackbarHost(snackbarHostState)}
     ) { paddingValues ->
         Column(
             modifier = modifier
@@ -77,9 +93,28 @@ fun AddEditDetailView(
             Button(onClick = {
                 // 입력된 값이 있을 때만 업데이트 하기
                 if (viewModel.wishTitleState.isNotEmpty() && viewModel.wishDescriptionState.isNotEmpty()) {
-                    // update wish func
+                    // add wish func
+                    if (id != 0L) {
+                        // update wish
+
+                    } else {
+                        // add wish
+                        viewModel.addWish(
+                            Wish(
+                                title = viewModel.wishTitleState.trim(),
+                                description = viewModel.wishDescriptionState.trim()
+                            )
+                        )
+                        snackMessage = "adding complete"
+                    }
                 } else {
-                    // add wish
+                   snackMessage = "Enter fileds to create a wish"
+                }
+                // 저장 or 업데이트가 끝나면 이전 페이지로 자동 이동하도록
+                scope.launch {
+                    // 스낵바를 띄움
+                    snackbarHostState.showSnackbar(snackMessage)
+                    navController.navigateUp()
                 }
             }) {
                 Text(
