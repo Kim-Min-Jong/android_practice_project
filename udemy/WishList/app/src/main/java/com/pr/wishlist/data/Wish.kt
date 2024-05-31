e131e854e583491fb025ad3c30dd56f3b8e1ec3a
minjong kim <alswhddkdl@gmail.com>
2024-05-31 09:50:38 +0900

    [Wish List] feat: setting up the entity for wish data class

--- udemy/WishList/app/src/main/java/com/pr/wishlist/data/Wish.kt
+++ udemy/WishList/app/src/main/java/com/pr/wishlist/data/Wish.kt
@@ -1,8 +1,17 @@
 package com.pr.wishlist.data
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+// Room 사용을 위해 @Entity 적용
+@Entity(tableName="wish_table")
 data class Wish(
+    @PrimaryKey(autoGenerate = true)
     val id: Long = 0L,
+    @ColumnInfo(name = "wish_title")
     val title: String = "",
+    @ColumnInfo(name = "wish_desc")
     val description: String = ""
 )
 
