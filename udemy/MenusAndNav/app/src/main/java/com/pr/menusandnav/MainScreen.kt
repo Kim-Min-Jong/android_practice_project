b192e411009e33ecde5799deb7fe1758711109f3
minjong kim <alswhddkdl@gmail.com>
2024-06-03 12:21:52 +0900

    [Menus&Nav] feat: adding resource icons & prepping drawerItem for drawer

--- udemy/MenusAndNav/app/src/main/java/com/pr/menusandnav/MainScreen.kt
+++ udemy/MenusAndNav/app/src/main/java/com/pr/menusandnav/MainScreen.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material3.DrawerValue
@@ -12,16 +14,23 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -37,25 +46,55 @@ fun MainScreen(
     val scaffoldState = rememberDrawerState(initialValue = DrawerValue.Closed)
     val scope = rememberCoroutineScope()
 
-    Scaffold(
-        // 상단바
-        topBar = {
-            TopAppBar(
-                title = { Text(text = "Home") },
-                navigationIcon = {
-                    IconButton(onClick = {
-                        // Drawer layout open
+    // 현재 화면 위치를 나타낼 변수
+    val controller = rememberNavController()
+    val navBackStackEntry by controller.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+
+    // 상단바 제목 유지 변수
+    var title by remember { mutableStateOf("Home") }
+    ModalNavigationDrawer(
+        drawerState = scaffoldState,
+        drawerContent = {
+            LazyColumn(
+                modifier = Modifier.padding(16.dp)
+            ) {
+                items(screensInDrawer) { item ->
+                    DrawerItem(selected = currentRoute == item.dRoute, item = item) {
                         scope.launch {
-                            scaffoldState.open()
+                            scaffoldState.close()
                         }
-                    }) {
-                        Icon(imageVector = Icons.Default.AccountCircle, contentDescription = null)
+                        if (item.dRoute == "add_account") {
+                            // 추가 다이얼로그 열기
+                        } else {
+                            controller.navigate(item.route)
+                            title = item.dTitle
+                        }
                     }
                 }
-            )
+            }
+        }) {
+        // mainContent
+        Scaffold(
+            // 상단바
+            topBar = {
+                TopAppBar(
+                    title = { Text(text = title) },
+                    navigationIcon = {
+                        IconButton(onClick = {
+                            // Drawer layout open
+                            scope.launch {
+                                scaffoldState.open()
+                            }
+                        }) {
+                            Icon(imageVector = Icons.Default.AccountCircle, contentDescription = null)
+                        }
+                    },
+                )
+            },
+        ) { paddingValues ->
+            Text(text = "text", modifier = modifier.padding(paddingValues))
         }
-    ) { paddingValues ->
-        Text(text = "text", modifier = modifier.padding(paddingValues))
     }
 }
 
@@ -73,7 +112,8 @@ fun DrawerItem(
 
     val backgroundColor = if (selected) Color.DarkGray else Color.White
     Row(
-        modifier = modifier.fillMaxWidth()
+        modifier = modifier
+            .fillMaxWidth()
             .padding(horizontal = 8.dp, vertical = 16.dp)
             .background(backgroundColor)
             .clickable {
