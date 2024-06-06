3258304d10761c9928921613929eec741f0738a8
minjong kim <alswhddkdl@gmail.com>
2024-06-06 14:20:31 +0900

    [Chatting Room] feat: creating alertDialog for new room

--- udemy/ChattingRoom/app/src/main/java/com/pr/chattingroom/screen/ChatRoomScreen.kt
+++ udemy/ChattingRoom/app/src/main/java/com/pr/chattingroom/screen/ChatRoomScreen.kt
@@ -1,24 +1,34 @@
 package com.pr.chattingroom.screen
 
-import android.widget.Space
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.DialogProperties
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ChatRoomScreen(
     modifier: Modifier = Modifier
@@ -48,6 +58,61 @@ fun ChatRoomScreen(
         // 새 방을 만드는 버튼
         Button(onClick = { showDialog = true }) {
             Text(text = "Create Room")
+        }
+
+        // 상태에 따른 다이얼로그 출력
+        if (showDialog) {
+            AlertDialog(
+                onDismissRequest = { showDialog = true },
+                properties = DialogProperties()
+            ) {
+                Card(
+                    shape = RoundedCornerShape(10)
+                ) {
+                    Column(
+                        horizontalAlignment = Alignment.Start,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        // 다이얼로그 제목
+                        Text(
+                            text = "Create a new room",
+                            modifier = modifier.padding(8.dp)
+                        )
+                        // 본문
+                        OutlinedTextField(
+                            value = name,
+                            onValueChange = { name = it },
+                            singleLine = true,
+                            modifier = modifier
+                                .fillMaxWidth()
+                                .padding(8.dp)
+                        )
+                        // button set
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(8.dp),
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
+                            Button(
+                                onClick = {
+                                    if (name.isNotBlank()) {
+                                        showDialog = false
+                                    }
+                                }
+                            ) {
+                                Text("Add")
+                            }
+                            Button(
+                                onClick = { showDialog = false }
+                            ) {
+                                Text("Cancel")
+                            }
+                        }
+                    }
+
+                }
+            }
         }
     }
 }
