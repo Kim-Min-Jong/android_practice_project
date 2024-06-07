b701418db9ed079864a7d762352c3c534729a3be
minjong kim <alswhddkdl@gmail.com>
2024-06-07 12:11:06 +0900

    [Chatting Room] feat: creating ChatItem Composable (with Message Class)

--- udemy/ChattingRoom/app/src/main/java/com/pr/chattingroom/screen/ChatScreen.kt
+++ udemy/ChattingRoom/app/src/main/java/com/pr/chattingroom/screen/ChatScreen.kt
@@ -1,16 +1,24 @@
 package com.pr.chattingroom.screen
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Send
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -18,11 +26,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.pr.chattingroom.R
+import com.pr.chattingroom.data.Message
+import com.pr.chattingroom.util.formatTimeStamp
 
-@@Composable
+@Composable
 fun ChatScreen(
     modifier: Modifier = Modifier
 ) {
@@ -68,6 +81,57 @@ fun ChatScreen(
             ) {
                 Icon(imageVector = Icons.Default.Send, contentDescription = "Send")
             }
+        }
+    }
+}
+
+// 각 채팅을 보여줄 컴포저블
+@RequiresApi(Build.VERSION_CODES.O)
+@@Composable
+fun ChatMessageItem(
+    modifier: Modifier = Modifier,
+    message: Message
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        // 상대방이냐 나냐에 따라 좌우 배치 변경
+        horizontalAlignment = if (message.isSentByCurrentUser) Alignment.End else Alignment.Start
+    ) {
+        Box(
+            modifier = modifier
+                .background(
+                    // 상대방이냐 나냐에 따라 배경색 변경
+                    if (message.isSentByCurrentUser) colorResource(id = R.color.purple_700)
+                    else Color.Gray,
+                    // 코너 둥글게
+                    shape = RoundedCornerShape(8.dp)
+                )
+                .padding(8.dp)
+        ) {
+            Text(
+                text = message.text,
+                color = Color.White,
+                style = TextStyle(fontSize = 16.sp)
+            )
+            Spacer(modifier = modifier.height(4.dp))
+            // 보낸 사람 이름
+            Text(
+                text = message.senderFirstName,
+                style = TextStyle(
+                    fontSize = 12.sp,
+                    color = Color.Gray
+                )
+            )
+            // 보낸 시간
+            Text(
+                text = formatTimeStamp(message.timestamp), // Replace with actual timestamp logic
+                style = TextStyle(
+                    fontSize = 12.sp,
+                    color = Color.Gray
+                )
+            )
         }
     }
 }
